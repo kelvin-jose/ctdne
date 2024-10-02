23ce2f04309d207a1c4e94121d2947249c860d9c
Kelvin <kelvinkonoor@gmail.com>
2024-10-02 21:34:37 +0530

    creating a graph from the read edge info

--- ctdne/preprocess_data.py
+++ ctdne/preprocess_data.py
@@ -6,3 +6,18 @@ from tqdm import tqdm
 seed = 42
 random.seed(seed)
 
+dataset = '../datasets/collegemsg/collegemsg_raw.txt'
+
+edges = []
+
+with open(dataset, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        edge = line.split()
+        start, end, timestamp = int(edge[0]), int(edge[1]), eval(edge[2])
+        edges.append((start, end, {'timestamp': timestamp}))
+
+# creating a graph from the read edge info
+graph = nx.MultiDiGraph()
+graph.add_edges_from(edges)
+
