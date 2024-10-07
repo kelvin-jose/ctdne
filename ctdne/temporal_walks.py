3d9b4381f8c589ae6db07acd2f95f63e6b90ac7e
Kelvin <kelvinkonoor@gmail.com>
2024-10-07 22:59:34 +0530

    temporal walk algorithm implementation

--- ctdne/temporal_walks.py
+++ ctdne/temporal_walks.py
@@ -19,3 +19,27 @@ N = len(graph.nodes)
 omega = 10 # context window size
 L = 80 # max length of walk
 beta = 25 * N * (L - omega + 1)
+
+C = 0
+temporal_walks = []
+edges = list(graph.edges(data=True))
+
+while beta - C > 0:
+    walk = []
+    first_edge = random.sample(edges, k=1) # unbiased first edge 
+    u, v = first_edge[0][:2]
+    walk.append(u)
+    walk.append(v)
+    vtime = first_edge[0][2]['timestamp']
+    
+    for i in range(L - 2):
+        vneighbors = list(graph.out_edges(v, data=True))
+        temp_neighbors = list(filter(lambda x: x[2]['timestamp'] >= vtime, vneighbors))
+        if temp_neighbors:
+            next_edge = random.sample(temp_neighbors, k=1)[0]
+            v, vtime = next_edge[1], next_edge[2]['timestamp']
+            walk.append(v)
+            
+    if len(walk) >= omega:
+        temporal_walks.append(walk)
+        C += len(walk) - omega + 1
