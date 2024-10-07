14fd78fc8c212294145b9199290d7c00571cc8e5
Kelvin <kelvinkonoor@gmail.com>
2024-10-07 22:58:16 +0530

    graph from edge info

--- ctdne/temporal_walks.py
+++ ctdne/temporal_walks.py
@@ -7,6 +7,10 @@ random.seed(seed)
 
 train_edges_file = '../datasets/collegemsg/train_edges.pkl'
 
+# loading edge info
 with open(train_edges_file, 'rb') as te:
     train_edges = pickle.load(te)
 
+# graph from edge info
+graph = nx.MultiDiGraph()
+graph.add_edges_from(train_edges)
