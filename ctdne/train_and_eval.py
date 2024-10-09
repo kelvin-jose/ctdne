1ad68629cf41cf694cbc0c6df04477a8b84134f0
Kelvin <kelvinkonoor@gmail.com>
2024-10-09 10:50:19 +0530

    actual word2vec model training

--- ctdne/train_and_eval.py
+++ ctdne/train_and_eval.py
@@ -7,14 +7,16 @@ temporal_walks_file = '../datasets/collegemsg/temporal_walks.pkl'
 with open(temporal_walks_file, 'rb') as file:
     temporal_walks = pickle.load(file)
 
-#word2vec model training
+# word2vec model configs
 embed_dim = 512
 window = 10
 min_count = 1
 workers = 4
 
+# actual word2vec model training
 w2v = Word2Vec(sentences = temporal_walks, 
               vector_size = embed_dim, 
               window = window, 
               min_count = min_count, 
               workers = workers)
+
