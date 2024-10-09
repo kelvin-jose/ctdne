04875a421fce0037ee819193369473196b9875aa
Kelvin <kelvinkonoor@gmail.com>
2024-10-09 11:01:27 +0530

    loading data for evalution

--- ctdne/train_and_eval.py
+++ ctdne/train_and_eval.py
@@ -20,3 +20,11 @@ w2v = Word2Vec(sentences = temporal_walks,
               min_count = min_count, 
               workers = workers)
 
+# loading data for evalution
+pos_edges_file = '../datasets/collegemsg/positive_edges.pkl'
+neg_edges_file = '../datasets/collegemsg/negative_edges.pkl'
+
+with open(pos_edges_file, 'rb') as pfile, open(neg_edges_file, 'rb') as nfile:
+    pos_edges = pickle.load(pfile)
+    neg_edges = pickle.load(nfile)
+
