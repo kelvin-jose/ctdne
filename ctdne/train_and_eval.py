ed4df3466be775455cecabb35c39ea1a07bcad9b
Kelvin <kelvinkonoor@gmail.com>
2024-10-09 10:48:30 +0530

    #word2vec model config

--- ctdne/train_and_eval.py
+++ ctdne/train_and_eval.py
@@ -1,7 +1,20 @@
 import pickle
+from gensim.models import Word2Vec
 
 temporal_walks_file = '../datasets/collegemsg/temporal_walks.pkl'
 
 # loading temporal walks
 with open(temporal_walks_file, 'rb') as file:
     temporal_walks = pickle.load(file)
+
+#word2vec model training
+embed_dim = 512
+window = 10
+min_count = 1
+workers = 4
+
+w2v = Word2Vec(sentences = temporal_walks, 
+              vector_size = embed_dim, 
+              window = window, 
+              min_count = min_count, 
+              workers = workers)
